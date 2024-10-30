4c3a6f39c0580084342efc1d53f57ce0bc889fbd
Jampala Rishi Krishna <76245234+Rishi123-png23@users.noreply.github.com>
2024-10-30 23:41:55 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -37,25 +37,13 @@ st.set_page_config(
 
 
 #hiding
-
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
-
-
-"""
 # use local css
 def local_css(file_name):
     with open(file_name) as f :
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 local_css("style/style.css")
-"""
+
 
 
 
